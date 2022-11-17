e1fa1748ebe2bbebba7c31e679c57dd45c234f71
Mordomocodificado <davixbuzato@gmail.com>
2022-11-17 17:07:00 -0300

    front post

--- createmodel.ps1
+++ createmodel.ps1
@@ -1,5 +1,5 @@
 $strconn = "Data Source=" + $args[0] + ";Initial Catalog=" + $args[1] + ";Integrated Security=True"
-dotnet add package Microsoft.EntityFrameworkCore.Design
+dotnet add package Microsoft.EntityFrameworkCore.Design -v 5.0.11
 dotnet add package Microsoft.EntityFrameworkCore.SqlServer
 dotnet tool install --global dotnet-ef
 dotnet ef dbcontext scaffold $strconn Microsoft.EntityFrameworkCore.SqlServer --force -o Model
